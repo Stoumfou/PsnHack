929d0d7e1e1c430d578db21e3ad60bd604dc99dd
Bouveti <bouveti94@gmail.com>
2016-03-30 11:45:58 +0200

    NIQUE TOI

---
+++ Hack/Mastermind/mastermind.js
@@ -0,0 +1,97 @@
+var code = [0,0,0,0];
+var input = [0,0,0,0];
+var result = [0,0,0,0];
+var codeCheck = [0,0,0,0];
+
+for(var i=0; i<4; i++){
+	code[i] = Math.floor(Math.random() * 10);
+}
+
+
+function play(){
+	var display = 'X X X X';
+    var tried = '- - - -';
+	
+	$('#res').text(display);
+    $('#tried').text(tried);
+		
+	$('#try').on('click', function(){
+		codeCheck = [0,0,0,0];
+		display = '';
+        
+        
+		for(var i=0; i<4; i++){
+			var selector = '#select'+i;
+			
+			input[i] = $(selector).val();
+				
+			result[i] = checkCode(input[i],i);
+        }
+        
+		for(var i=0; i<4; i++){
+			var selector = '#select'+i;
+			
+			input[i] = $(selector).val();
+				
+			result[i] = checkCode(input[i],i);
+				
+			switch(result[i]){
+				case -1: 	display +='M ';
+							break;
+					
+				case 1:		display += 'O ';
+							break;
+								
+				case 0:		display += 'X ';
+				default:;
+			}
+		}
+					
+		$('#res').text(display);		
+			
+		if(win())alert("VICTOIRE !\nLe code est:"+code[0]+code[1]+code[2]+code[3]);
+			
+		
+    $('#tried').text(""+input[0]+" "+input[1]+" "+input[2]+" "+input[3]);
+        
+    $('#res').removeAttr('id');
+    $('#tried').removeAttr('id');    
+    $('#game').append('<div class="col-lg-3" id="tried"></div><div class="col-lg-3" id="res"></div><div class="col-lg-6">&nbsp;</div>');
+	});		
+}
+
+function checkCode(value, pos){
+	var res = 0;
+	
+	if(value == code[pos]){
+		codeCheck[pos] = 1;
+		res = 1;
+	}
+	else{
+		
+		for(var i=0; i <4; i++){
+			if ((value == code[i]) && (codeCheck[i] == 0)) res = -1;
+		}	
+	}
+    //console.log(''+codeCheck[0]+codeCheck[1]+codeCheck[2]+codeCheck[3]);
+	return res;
+}
+
+function win(){
+	res = true;
+	
+	for(var i=0; i<4; i++)if(result[i] != 1)res = false;
+	
+	return res;
+}
+
+$(document).ready(function(){
+    var select = '';
+    for(var i=0; i<4; i++){
+        select += '<select id="select'+i+'"><option value="0">0</option><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option></select>';
+    }
+    
+	$('#game').append('<div class="col-lg-3" id="tried"></div><div class="col-lg-3" id="res"></div><div class="col-lg-6"><label>Input:</label>'+select+'<button id="try">Try</button></div>');
+    
+	play();	
+});
